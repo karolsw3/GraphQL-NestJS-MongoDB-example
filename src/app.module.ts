03c059c4e5e06877db5fc81c306bafafc2ee4a02
Charles Spruce <karol.sw3@gmail.com>
2020-05-20 08:37:34 +0200

    feat: add GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    GraphQLModule.forRoot({
+    playground: true,
+    autoSchemaFile: 'schema.qgl'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
